75c61707fd05df18e4ed6d37619dd658389fcedf
Andro Rezkalla <78698964+androrezkalla@users.noreply.github.com>
2023-06-28 16:19:29 -0400

    updated DOM routing

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { FaHome, FaInfoCircle, FaList, FaChartBar } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
   const [collapsed, setCollapsed] = useState(true);
@@ -9,7 +10,11 @@ const Sidebar = () => {
   };
 
   return (
-    <div className={`bg-gray-800 h-screen ${collapsed ? 'w-16' : 'w-45'} fixed top-24 left-0 flex flex-col items-center transition-all duration-300`}>
+    <div
+      className={`bg-gray-800 h-screen ${
+        collapsed ? 'w-16' : 'w-45'
+      } fixed top-24 left-0 flex flex-col items-center transition-all duration-300`}
+    >
       <button
         className="text-white p-4 focus:outline-none hover:bg-gray-700"
         onClick={handleToggleCollapse}
@@ -18,24 +23,24 @@ const Sidebar = () => {
       </button>
       {!collapsed && (
         <>
-          <a href="/" className="text-white p-4 hover:bg-gray-700 flex items-center">
+          <Link to="/" className="text-white p-4 hover:bg-gray-700 flex items-center">
             <FaHome size={24} />
             <span className="ml-2">Home</span>
-          </a>
-          <a href="/charts" className="text-white p-4 hover:bg-gray-700 flex items-center">
+          </Link>
+          <Link to="/charts" className="text-white p-4 hover:bg-gray-700 flex items-center">
             <FaChartBar size={24} />
             <span className="ml-2">Statistics</span>
-          </a>
+          </Link>
         </>
       )}
       {collapsed && (
         <>
-          <a href="/" className="text-white p-4 hover:bg-gray-700">
+          <Link to="/" className="text-white p-4 hover:bg-gray-700">
             <FaHome size={24} />
-          </a>
-          <a href="/charts" className="text-white p-4 hover:bg-gray-700">
+          </Link>
+          <Link to="/charts" className="text-white p-4 hover:bg-gray-700">
             <FaChartBar size={24} />
-          </a>
+          </Link>
         </>
       )}
     </div>
